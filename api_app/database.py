c04b41b6fa94a8e4de199b3320faedc3c695f59e
Bear-Asus <mtaran@ukr.net>
2024-11-27 12:37:22 +0200

    add_FastAPI/PostgreSQL

--- api_app/database.py
+++ api_app/database.py
@@ -1,13 +1,13 @@
 import os
-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from dotenv import load_dotenv
 load_dotenv()
-
+# подключение базы данных PostgreSQL
 SQLALCHEMY_DATABASE_URL = os.getenv("POSTGRESQL_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 conn = engine.connect()
 print(conn.get_isolation_level())
+#
