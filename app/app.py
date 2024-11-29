e424e37b9a78f82403f3591e5162ecb40f411381
Mike Taran <mtaran@ukr.net>
2024-11-29 18:53:01 +0200

    FastAPI/File_convertor

--- app/app.py
+++ app/app.py
@@ -5,7 +5,7 @@ from fastapi import Depends, FastAPI
 from app.db import User, create_db_and_tables
 from app.schemas import UserCreate, UserRead, UserUpdate
 from app.users import auth_backend, current_active_user, fastapi_users
-
+from app.files import file_router
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -28,6 +28,7 @@ app.include_router(
     fastapi_users.get_reset_password_router(),
     prefix="/auth",
     tags=["auth"],
+
 )
 app.include_router(
     fastapi_users.get_verify_router(UserRead),
@@ -38,6 +39,13 @@ app.include_router(
     fastapi_users.get_users_router(UserRead, UserUpdate),
     prefix="/users",
     tags=["users"],
+)
+
+
+app.include_router(
+    file_router,
+    prefix="files",
+    tags=["files"]
 )
 
 
--- app/schemas.py
+++ app/schemas.py
@@ -1,7 +1,7 @@
 import uuid
-
+from pydantic import BaseModel, Field
 from fastapi_users import schemas
-
+from typing import Optional
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
     pass
@@ -13,3 +13,10 @@ class UserCreate(schemas.BaseUserCreate):
 
 class UserUpdate(schemas.BaseUserUpdate):
     pass
+
+
+class ImageProcessionOptions(BaseModel):
+    resize: Optional[str] = Field(None, description="Example: 1980x1200")
+    convert_to: Optional[str] = Field(None, description="png, jpg, webp")
+    grayscale: Optional[bool] = Field(None, description="Convert to greyscale")
+    flip: Optional[str] = Field(None, description="horizontal or vertical")
